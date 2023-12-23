5e1a601326f817229698daae2f2da728b1cfab3d
jonpojonpo <jonpojonpo@users.noreply.github.com>
2023-12-23 10:08:40 +0000

    Create app.py
    initial API

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import subprocess
+import os
+
+app = Flask(__name__)
+BEARER_TOKEN = os.environ.get('BEARER_TOKEN')
+
+def verify_token(token):
+    return token == BEARER_TOKEN
+
+@app.route('/run-command', methods=['POST'])
+def run_command():
+    if not verify_token(request.headers.get('Authorization')):
+        return jsonify({'error': 'Unauthorized'}), 401
+
+    data = request.json
+    command = data.get('command')
+    
+    # Security check: Validate and sanitize the command here
+
+    try:
+        output = subprocess.check_output(command, shell=True, text=True)
+        return jsonify({'output': output})
+    except subprocess.CalledProcessError as e:
+        return jsonify({'error': str(e), 'output': e.output})
+
+if __name__ == '__main__':
+    app.run()
