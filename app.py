5fbfa23852d0b23518e607221c817a4e79b243d0
CAN DO <cando@example.com>
2023-12-27 13:04:03 +0000

    added patch apply

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import tempfile
 import subprocess
 import os
 
@@ -27,7 +28,6 @@ def run_command():
     except subprocess.CalledProcessError as e:
         return jsonify({'error': str(e), 'output': e.output})
 
-# New route for applying a unified patch
 @app.route('/apply-patch', methods=['POST'])
 def apply_patch():
     # Verify the authorization token
@@ -42,12 +42,23 @@ def apply_patch():
     if not file_path or not patch_content:
         return jsonify({'error': 'Missing file_path or patch_content'}), 400
 
-    # Apply the patch using 'git apply'
+    # Write the patch content to a temporary file and apply it
     try:
-        subprocess.run(['git', 'apply', '--cached', '--whitespace=nowarn', patch_content], check=True)
+        with tempfile.NamedTemporaryFile(mode='w+', delete=False) as tmp_file:
+            tmp_file.write(patch_content)
+            tmp_file_path = tmp_file.name
+
+        subprocess.run(['git', 'apply', '--whitespace=nowarn', tmp_file_path], check=True)
+
         return jsonify({'message': 'Patch applied successfully'})
     except subprocess.CalledProcessError as e:
         return jsonify({'error': 'Failed to apply patch: ' + str(e)})
+    finally:
+        # Clean up the temporary file
+        try:
+            os.remove(tmp_file_path)
+        except OSError:
+            pass  # Handle error if file doesn't exist
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
