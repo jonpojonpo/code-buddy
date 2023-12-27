e41f0404165335405be9d329e5cd2f952b24bcac
CAN DO <cando@example.com>
2023-12-27 11:51:26 +0000

    added patch route

--- app.py
+++ app.py
@@ -8,19 +8,16 @@ BEARER_TOKEN = os.environ.get('BEARER_TOKEN')
 def verify_token(token):
     parts = token.split()
     if len(parts) == 2 and parts[0] == 'Bearer':
-            token = parts[1]
-    print( token + "==" + BEARER_TOKEN + "?")
+        token = parts[1]
     return token == BEARER_TOKEN
 
 @app.route('/run-command', methods=['POST'])
 def run_command():
-    #print(request.headers.get('Authorization'))
     if not verify_token(request.headers.get('Authorization')):
         return jsonify({'error': 'Unauthorized'}), 401
 
     data = request.json
     command = data.get('command')
-    print(command)
     
     # Security check: Validate and sanitize the command here
 
@@ -30,5 +27,28 @@ def run_command():
     except subprocess.CalledProcessError as e:
         return jsonify({'error': str(e), 'output': e.output})
 
+# New route for applying a unified patch
+@app.route('/apply-patch', methods=['POST'])
+def apply_patch():
+    # Verify the authorization token
+    if not verify_token(request.headers.get('Authorization')):
+        return jsonify({'error': 'Unauthorized'}), 401
+
+    data = request.json
+    file_path = data.get('file_path')
+    patch_content = data.get('patch_content')
+
+    # Check if file_path and patch_content are provided
+    if not file_path or not patch_content:
+        return jsonify({'error': 'Missing file_path or patch_content'}), 400
+
+    # Apply the patch using 'git apply'
+    try:
+        subprocess.run(['git', 'apply', '--cached', '--whitespace=nowarn', patch_content], check=True)
+        return jsonify({'message': 'Patch applied successfully'})
+    except subprocess.CalledProcessError as e:
+        return jsonify({'error': 'Failed to apply patch: ' + str(e)})
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
